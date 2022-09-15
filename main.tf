3f060331b69ffaa9b88bdd0715315f41f5a52214
Prabhakar Pratim Borah <prabhakar.borah@gmail.com>
2022-09-15 16:08:19 +0530

    updated for backend block for remote backend store
    AWS S3 used as backend for terraform state

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "ppb-tf-state-store"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "tf-state-lock"
+  }
+}
+
 resource "aws_s3_bucket" "test-bucket" {
   bucket = "very-random-ppb-bucket-1"
 }
