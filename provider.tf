7b03deb0c81ed1687d43482f67b8a7853f93ad16
Prabhakar Pratim Borah <prabhakar.borah@gmail.com>
2022-09-15 17:11:54 +0530

    Update provider.tf
    removed aws credentials

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  access_key                  = "AKIASLTF4IZ4HROAGT47"
-  secret_key                  = "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"
   region                      = "ap-south-1"
 }
