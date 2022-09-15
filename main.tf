999405d9d96b4a889b800c905b954985a9d06a42
Prabhakar Pratim Borah <prabhakar.borah@gmail.com>
2022-09-15 15:44:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
 resource "aws_s3_bucket" "test-bucket" {
   bucket = "very-random-ppb-bucket-1"
 }
-
-resource "aws_iam_role" "invocation_role" {
-  name = "api_gateway_auth_invocation"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Action": "s3:ListBucket",
-    "Resource": "arn:aws:s3:::very-random-ppb-bucket-1"
-  }
-}
-EOF
-}
