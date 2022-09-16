3419319531110e4fde2ae7e5d3b65a45ebabd742
Prabhakar Pratim Borah <prabhakar.borah@gmail.com>
2022-09-16 15:06:38 +0530

    updating for security group

--- main.tf
+++ main.tf
@@ -114,22 +114,22 @@ resource "aws_security_group" "default" {
   name        = "${var.environment}-default-sg"
   description = "Default SG to alllow traffic from the VPC"
   vpc_id      = aws_vpc.vpc.id
-  depends_on = [
-    aws_vpc.vpc
-  ]
+  #depends_on = [
+  #  aws_vpc.vpc
+  #]
 
     ingress {
     from_port = "0"
     to_port   = "0"
     protocol  = "-1"
-    self      = true
+    #self      = "true"
   }
 
   egress {
     from_port = "0"
     to_port   = "0"
     protocol  = "-1"
-    self      = "true"
+    #self      = "true"
   }
 
   tags = {
