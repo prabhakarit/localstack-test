69a0163c36035a2f4f3f33d2e8a65f760c168b6f
Prabhakar Pratim Borah <prabhakar.borah@gmail.com>
2022-09-16 11:49:21 +0530

    added vars.tf for vpc, subnets setup

---
+++ vars.tf
@@ -0,0 +1,52 @@
+##############################################
+## Network Single AZ Public Only - Variables #
+##############################################
+
+variable "environment" {
+  type        = string
+  description = "environment"
+  default = "dev"
+}
+
+# AWS AZ
+variable "availability_zones" {
+  type        = string
+  description = "AWS AZ a ?"
+  default     = "ap-south-1a"
+}
+
+# VPC Variables
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+# Subnet Variables
+variable "public_subnets_cidr" {
+  type        = string
+  description = "CIDR for the public subnet"
+  default     = "10.0.2.0/24"
+}
+
+# Subnet Variables
+variable "private_subnets_cidr" {
+  type        = string
+  description = "CIDR for the private subnet"
+  default     = "10.0.3.0/24"
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
