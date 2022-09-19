313c3986d214262a04de3e6604243e4132acf9bc
prabhakarit <prabhakar.borah@gmail.com>
2022-09-19 20:33:02 +0530

    updated config for backend

--- terraform-config/localstack/backend.tf
+++ terraform-config/localstack/backend.tf
@@ -1,8 +1,6 @@
-terraform {
-  backend "s3" {
+backend "s3" {
     bucket         = "ppb-tf-state-store"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "ppb-tf-state-lock"
   }
-}
